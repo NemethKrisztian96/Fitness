7145e7a2109a8823532ab3465d1fae0f9156d6c3
NemethKrisztian96 <n.krisztian96@gmail.com>
2019-05-08 22:25:04 +0300

    Added advanced tab management and partially finished home page

---
+++ Common/FitnessTabContents/IHomeContent.cs
@@ -0,0 +1,14 @@
+﻿namespace Fitness.Common.FitnessTabContents
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IHomeContent : IFitnessContent
+    {
+        bool IsAdmin { get; }
+        int UserId { get; }
+    }
+}
--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,11 @@
-﻿using Fitness.Common.MVVM;
+﻿using Fitness.Common.FitnessTabContents;
+using Fitness.Common.Helpers;
+using Fitness.Common.MVVM;
+using Fitness.Model;
+using Fitness.ViewModel.UserControls;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +14,57 @@ namespace Fitness.ViewModel
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public MainWindowViewModel()
+        private ObservableCollection<IFitnessContent> contents;
+        private IFitnessContent selectedContent;
+        private static readonly MainWindowViewModel instance = new MainWindowViewModel();
+        private bool isSignedIn = true;
+
+        private User signedInUser;
+        public User SignedInUser {
+            get => signedInUser;
+            set {
+                signedInUser = value;
+                if (signedInUser != null)
+                {
+                    this.IsSignedIn = true;
+                }
+                else
+                {
+                    this.IsSignedIn = false;
+                }
+                this.RaisePropertyChanged();
+            }
+        }
+
+        static MainWindowViewModel() { }
+
+        private MainWindowViewModel()
         {
-
-
             this.CloseCommand = new RelayCommand(this.CloseCommandExecute);
+            this.LogOutCommand = new RelayCommand(this.LogOutCommandExecute);
+
+            this.GenerateContents();
+        }
+
+        public bool IsSignedIn
+        {
+            get
+            {
+                return this.isSignedIn;
+            }
+            set
+            {
+                this.isSignedIn = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public static MainWindowViewModel Instance
+        {
+            get
+            {
+                return instance;
+            }
         }
 
         public RelayCommand CloseCommand { get; private set; }
@@ -21,6 +72,62 @@ namespace Fitness.ViewModel
         public void CloseCommandExecute()
         {
             ViewService.CloseDialog(this);
+        }
+
+        public RelayCommand LogOutCommand { get; private set; }
+
+        public void LogOutCommandExecute()
+        {
+            this.LogOutDialog(this);
+        }
+
+        public ObservableCollection<IFitnessContent> Contents
+        {
+            get
+            {
+                return this.contents;
+            }
+            set
+            {
+                this.contents = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public IFitnessContent SelectedContent
+        {
+            get
+            {
+                return this.selectedContent;
+            }
+            set
+            {
+                this.selectedContent = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        private void GenerateContents()
+        {
+            this.Contents = new ObservableCollection<IFitnessContent>();
+            IHomeContent homeViewModel = new HomeViewModel();
+            this.contents.Add(homeViewModel);
+
+            this.SelectedContent = this.Contents.First();  //not empty => no exception
+        }
+
+        public void CloseTabItem(IFitnessContent content)
+        {
+            if (content != null)
+            {
+                this.selectedContent = this.Contents.FirstOrDefault();
+                this.Contents.Remove(content);
+            }
+        }
+
+        public void LogOutDialog(ViewModelBase viewModel)
+        {
+            this.SignedInUser = null;
         }
     }
 }
