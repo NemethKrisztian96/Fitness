a27972c6e572662bde3436ff39d1afec0e7c35c4
Fulop Timea <fuloptimea1427@gmail.com>
2019-05-10 16:03:46 +0300

    added usernames

--- Model/User.cs
+++ Model/User.cs
@@ -6,6 +6,7 @@ namespace Fitness.Model
     {
         [Key]
         public int Id { get; set; }
+        public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Password { get; set; }
