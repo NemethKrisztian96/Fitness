6803a6ee450902faeb411f6b7a2daf46ff35ba2e
NemethKrisztian96 <n.krisztian96@gmail.com>
2019-05-16 17:17:46 +0300

    Added ListClients and option to delete client

--- View/TemplateSelectors/FitnessContentSelector.cs
+++ View/TemplateSelectors/FitnessContentSelector.cs
@@ -43,6 +43,10 @@ namespace Fitness.View.TemplateSelectors
             {
                 return Application.Current.MainWindow.TryFindResource("ClientTicketListTemplate") as DataTemplate;
             }
+            else if (item is IListClientsContent)
+            {
+                return Application.Current.MainWindow.TryFindResource("ListClientsTemplate") as DataTemplate;
+            }
             /*else if (item is )
             {
                 return Application.Current.MainWindow.TryFindResource("SomeTemplate") as DataTemplate;
--- ViewModel/UserControls/ListClientsViewModel.cs
+++ ViewModel/UserControls/ListClientsViewModel.cs
@@ -15,15 +15,37 @@ namespace Fitness.ViewModel.UserControls
         public string Header => "List of clients";
 
         public RelayCommand CloseTabItemCommand { get; set; }
+        public RelayCommand ManageClientCommand { get; set; }
+        public RelayCommand DeleteClientCommand { get; set; }
 
         public bool ShowCloseButton => true;
 
         public List<Client> ClientsList { get; set; }
+        public Client SelectedClient { get; set; }
 
         public ListClientsViewModel()
         {
             this.CloseTabItemCommand = new RelayCommand(this.CloseTabItemExecute);
             this.ClientsList = Data.Fitness.GetClients();
+            this.ManageClientCommand = new RelayCommand(this.ManageClientExecute);
+            this.DeleteClientCommand = new RelayCommand(this.DeleteClientExecute);
+        }
+
+        public void ManageClientExecute()
+        {
+            if (this.SelectedClient != null)
+            {
+                MainWindowViewModel.Instance.SetClientManageClientTab(this.SelectedClient);
+            }
+        }
+
+        public void DeleteClientExecute()
+        {
+            if (this.SelectedClient != null)
+            {
+                this.SelectedClient.IsDeleted = true;
+                Data.Fitness.SaveAllChanges();
+            }
         }
 
         public void CloseTabItemExecute()
