fcd4f30386d3fbfd0f14b9f15eaba0fc1bd6f5ac
NemethKrisztian96 <n.krisztian96@gmail.com>
2019-05-16 23:16:19 +0300

    Working email sending

--- ViewModel/UserControls/SendEmailViewModel.cs
+++ ViewModel/UserControls/SendEmailViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using Fitness.Common.FitnessTabContents;
+using Fitness.Common.Helpers;
 using Fitness.Common.MVVM;
 using Fitness.Logic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,17 +33,41 @@ namespace Fitness.ViewModel.UserControls
         public void SendEmailExecute()
         {
             string adminEmailAddress = "nkmiklos96@gmail.com";
-            List<string> emails = Data.Fitness.GetClientEmails();
-            MailMessage mail = new MailMessage();
-            foreach (string email_to in emails)
+            //List<string> emails = Data.Fitness.GetClientEmails();
+            List<string> emails = new List<string>();
+            emails.Add("fuloptimea1427@gmail.com");
+            try
             {
-                mail.To.Add(email_to);
+                using (MailMessage mail = new MailMessage())
+                {
+                    foreach (string email_to in emails)
+                    {
+                        mail.To.Add(email_to);
+                    }
+                    mail.Subject = Subject;
+                    mail.From = new MailAddress(adminEmailAddress);
+                    mail.Body = Body;
+
+                    //SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587)
+                    //{
+                    //    Credentials = new NetworkCredential(adminEmailAddress, "1234asdfQWER"),
+                    //    EnableSsl = true
+                    //};
+                    //smtp.Send(mail);
+
+                    using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        smtp.Credentials = new NetworkCredential(adminEmailAddress, "1234asdfQWER");
+                        smtp.EnableSsl = true;
+                        smtp.Send(mail);
+                        PopupMessage.OkButtonPopupMessage("Success", "The email has been sent");
+                    }
+                }
             }
-            mail.Subject = Subject;
-            mail.From = new MailAddress(adminEmailAddress);
-            mail.Body = Body;
-            SmtpClient smtp = new SmtpClient("SMTP Server");
-            smtp.Send(mail);
+            catch(Exception e) {
+                PopupMessage.OkButtonPopupMessage("Error", "We were unable to send your email");
+                Console.WriteLine(e.Message);
+            }
         }
 
         public void CloseTabItemExecute()
